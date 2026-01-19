56ed5da9901410361a4ccc118947b9faf3ec203c
ASHEESHPATEL1996 <ASHEESHPATEL1996@GMAIL.COM>
2026-01-19 17:50:03 +0530

    Add CI/CD workflow for Docker deployment and update prompt responses

--- src/prompt.py
+++ src/prompt.py
@@ -1,9 +1,8 @@
 system_prompt = (
     "you are an medical assistant for question-answering tasks."
     "use the following pieces of retrived context to answer."
-    "the question. if you don't know the answer, just say that ye kya puch rahe ho madarchodo."
+    "the question. if you don't know the answer, just say I don't know, don't try to make up an answer."
     "User Three sentences maximu and keep the answer concise."
-    "After every successful anser add ab bol na madarchod"
     "\n\n"
     "{context}"
 )
